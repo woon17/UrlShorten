480e83d2baaf3c66dfe2dabda599312f84d41d3a
WEN SHUFA <shufa17@gmail.com>
2021-08-16 11:07:28 +0800

    design a simple page

--- shortener/views.py
+++ shortener/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Shortener
+import requests, json
 
 # Create your views here.
 from django.http import HttpResponse
 
+DOMAIN1="http://127.0.0.1:8000/"
+DOMAIN2="http://localhost:8000/"
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    print("--------------------")
+    try:
+        print(request.body)
+        longUrl = request.POST.get('longUrl')
+        customShortenPart = request.POST.get('customShortenPart')
+        domain = request.POST.get('domain')
+        print(f'longUrl: {longUrl}; customShortenPart: {customShortenPart}; domain: {domain};')
+    except:
+        pass
+    return render(request, 'shortener/home.html', {"longUrl": longUrl, "customShortenPart": customShortenPart, "domains": [DOMAIN1, DOMAIN2], "domain": domain})
 
 def getLongUrl(shortenPart):
     print("------------------------")
