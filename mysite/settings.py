e43e64d7aed13dc6c80cceb589004cbfcea38fab
WEN SHUFA <shufa17@gmail.com>
2021-08-18 12:01:34 +0800

    edit secretekey access

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('ShortenUIRL_Django_secreteKey')
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
