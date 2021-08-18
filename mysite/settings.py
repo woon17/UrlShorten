001ea56894c19f6b943d6eb2fbbdc4ce124a1dd9
WEN SHUFA <shufa17@gmail.com>
2021-08-18 12:23:00 +0800

    edit debug mode and add host allowed

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
 
-ALLOWED_HOSTS = []
+if os.environ['DEBUG_VALUE'] is True:
+    DEBUG = True
+else:
+    DEBUG = False
+
+ALLOWED_HOSTS = ['myshortenurlapp.herokuapp.com']
 # ALLOWED_HOSTS = ["*"]
 
 
